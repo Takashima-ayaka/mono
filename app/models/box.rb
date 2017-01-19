2824192f83e17a886878136bcf306339da1efc66
student <monkastudent@gmail.com>
2017-01-19 06:53:17 +0000

    first commit

---
+++ app/models/box.rb
@@ -0,0 +1,5 @@
+class Box < ApplicationRecord
+    has_many :items
+    
+    validates :name, presence: true
+end
---
+++ app/models/item.rb
@@ -0,0 +1,6 @@
+class Item < ApplicationRecord
+    belongs_to :box, optional:true
+    
+    validates :name, presence: true
+    
+end
